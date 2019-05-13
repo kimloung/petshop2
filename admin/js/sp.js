7e642853a8d32fdb9f82a819699f753e7932a2ad
Đỗ Tường Đại <44903756+tuongdai252@users.noreply.github.com>
2019-05-13 21:30:14 +0700

    Add files via upload

---
+++ admin/js/sp.js
@@ -0,0 +1,46 @@
+﻿function popup_themsp() {
+    document.getElementsByClassName('popup-themsp')[0].style.display = 'block';
+}
+
+function close_popup_themsp() {
+    document.getElementsByClassName('popup-themsp')[0].style.display = 'none';
+	try {
+        document.getElementsByClassName('popup-themsp')[1].style.display = 'none';
+    } catch (e) {
+        console.log("Đang ở trang Quản lý Đơn Hàng");
+    }
+}
+
+function xoa_sp(){
+    if (confirm('Bạn có chắc chắn muốn xoá?')) {
+        if (confirm('Xác nhận xoá!!!'))
+            return true;
+        else
+            return false;
+    }
+    else
+        return false;
+}
+
+function khoiphuc_sp(){
+    if (confirm('Bạn có chắc chắn muốn khôi phục?')) {
+        return true;
+    }
+    else
+        return false;
+}
+
+function xoavinhvien_sp(){
+    if (confirm('Thao tác này sẽ xóa dữ liệu khỏi cơ sở dữ liệu. Bạn có chắc chắn muốn xoá?')) {
+        if (confirm('Thao tác này sẽ xóa dữ liệu khỏi cơ sở dữ liệu. Xác nhận xoá!!!')){
+            if (confirm('Thao tác này sẽ xóa dữ liệu khỏi cơ sở dữ liệu. Xác nhận xoá lần cuối!!!'))
+                return true;
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+    else
+        return false;
+}
